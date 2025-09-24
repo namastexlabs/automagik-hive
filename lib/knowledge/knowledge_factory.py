ba26d3e6b6121866b92b8b5f4b476d85652331a3
Automagik Genie <genie@namastex.ai>
2025-09-24 11:59:27 -0300

    hive-review to group C agno v2

--- lib/knowledge/knowledge_factory.py
+++ lib/knowledge/knowledge_factory.py
@@ -6,16 +6,9 @@ from importlib import import_module
 from typing import Any
 
 
-def _factory_module():
-    return import_module("lib.knowledge.factories.knowledge_factory")
-
-
 def get_knowledge_base(*args: Any, **kwargs: Any):
-    return _factory_module().get_knowledge_base(*args, **kwargs)
+    module = import_module("lib.knowledge.factories.knowledge_factory")
+    return module.get_knowledge_base(*args, **kwargs)
 
 
-def create_knowledge_base(*args: Any, **kwargs: Any):
-    return _factory_module().create_knowledge_base(*args, **kwargs)
-
-
-__all__ = ["create_knowledge_base", "get_knowledge_base"]
+__all__ = ["get_knowledge_base"]
